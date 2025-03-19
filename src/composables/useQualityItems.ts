6da26a96a2c1bf21bef895cc714186e3f1ffbfc5
iddk <ldw940321@gmail.com>
2025-03-19 11:26:29 +0900

    fix: make useQualityItems reactive by passing ref directly

--- src/composables/useQualityItems.ts
+++ src/composables/useQualityItems.ts
@@ -1,9 +1,10 @@
 import {useQuery} from "@tanstack/vue-query";
 import {fetchItems} from "@/services/api.ts";
+import type {Ref} from "vue";
 
-export const useQualityItems = (page: number) => {
+export const useQualityItems = (page: Ref<number>) => {
   return useQuery({
     queryKey: ['items', page],
-    queryFn: () => fetchItems(page),
+    queryFn: () => fetchItems(page.value),
   });
 }
