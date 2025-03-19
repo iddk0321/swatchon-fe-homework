6217d66c96a52c0aa64388505ed2822b1c710028
iddk <ldw940321@gmail.com>
2025-03-19 11:12:21 +0900

    feat: integrate Vue Query and configure global settings

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
 import './assets/main.scss'
+import { VueQueryPlugin, QueryClient} from "@tanstack/vue-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 createApp(App)
   .use(router)
+  .use(VueQueryPlugin, { queryClient })
   .mount('#app')
