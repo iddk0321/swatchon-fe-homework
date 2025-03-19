957458576597a79544ec5a1a9924f12557498bfe
iddk <ldw940321@gmail.com>
2025-03-19 12:22:29 +0900

    feat: Add addToCart function and CartQualityResponse type

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,6 +14,11 @@ export type QualityResponse  = {
   perPage: number;
 }
 
+export type CartQualityResponse = {
+  id: number
+  quality: Quality
+}
+
 export const fetchItems = async (page: number = 1): Promise<QualityResponse> => {
   try {
     const response = await fetch(`/api/items?page=${page}`)
@@ -23,6 +28,25 @@ export const fetchItems = async (page: number = 1): Promise<QualityResponse> => 
     return await response.json()
   } catch (error) {
     console.error('fetchFailed', error)
+    throw error
+  }
+}
+
+export const addToCart = async (qualityId: number): Promise<CartQualityResponse> => {
+  try {
+    const response = await fetch(`/api/cart_qualities`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ qualityId })
+    })
+    if (!response.ok) {
+      throw new Error(`addToCard failed`)
+    }
+    return await response.json()
+  } catch (error) {
+    console.error('addToCard failed', error)
     throw error
   }
 }
