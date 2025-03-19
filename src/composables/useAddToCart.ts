8be7311baaf32e237cb688a227652139623af1bd
iddk <ldw940321@gmail.com>
2025-03-19 13:00:11 +0900

    feat: update cart mutation to refresh cart badge count

--- src/composables/useAddToCart.ts
+++ src/composables/useAddToCart.ts
@@ -1,8 +1,14 @@
-import {useMutation} from "@tanstack/vue-query";
+import {useMutation, useQueryClient} from "@tanstack/vue-query";
 import {addToCart} from "@/services/api.ts";
 
 export const useAddToCart = () => {
+  const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: (qualityId: number) => addToCart(qualityId)
+    mutationFn: (qualityId: number) => addToCart(qualityId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['cartBadgeCount']
+      })
+    }
   })
 }
